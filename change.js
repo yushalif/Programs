1de4f5d20b89a2239d1319ad52033d1fa6aa556b
Yusha Alif <yusha.alif1@gmail.com>
2023-12-26 21:14:56 +0600

    remote pc test

--- change.js
+++ change.js
@@ -1,2 +1,3 @@
 console.log('Change')
 console.log('Version 2')
+console.log("remote pc pull test")
--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 console.log('config.js');
 console.log('version2');
+console.log("remote pc pull test")
